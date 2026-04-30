df13b33f57439c5128fe3d08491323c4c255d080
78759594 <shrikantnavale333@gamil.com>
2026-04-30 15:34:59 +0530

    some changes

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,9 +4,11 @@ from playwright.sync_api import sync_playwright
 
 def test_example():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://playwright.dev/")
-        assert "Playwright" in page.title()
+        title = page.title()
+        assert "Playwright" in title
+        print(title)
         time.sleep(5)
         browser.close()
