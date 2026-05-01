4a2108a1653ca89bcfddcc2a778e24f80d17fe2d
78759594 <shrikantnavale333@gamil.com>
2026-05-01 11:29:35 +0530

    some changes

--- tests/test_login_website.py
+++ tests/test_login_website.py
@@ -1,13 +1,16 @@
 import time
 
 import playwright
-from playwright.sync_api import Page, Playwright, expect
+from playwright.sync_api import Page, Playwright, expect, sync_playwright
+
 
 def test_login_web(page : Page ):
-
-    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
-    page.get_by_label("Username:").fill("rahulshettyacademy")
-    page.get_by_label("Password:").fill("Learning@830$3mK2")
-    page.locators("#terms").click()
-    page.get_by_role("button", name = "sign in").click()
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+        page.get_by_label("Username:").fill("rahulshettyacademy")
+        page.get_by_label("Password:").fill("Learning@830$3mK2")
+        page.locators("#terms").click()
+        page.get_by_role("button", name = "sign in").click()
 
