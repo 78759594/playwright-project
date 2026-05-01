2f712a99b69c00ee0d567e45fbd9fd96daddf290
78759594 <shrikantnavale333@gamil.com>
2026-05-01 17:31:36 +0530

    new chages

--- tests/test_login_website.py
+++ tests/test_login_website.py
@@ -27,6 +27,24 @@ def test_login_web():
         # to validate assertion with test case
         expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
 
+        page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+        # login test case
+        page.get_by_label("Username:").fill("rahulshettyacademy")
+        page.get_by_label("Password:").fill("Learning@830$3mK2")
+
+        # select checkbox
+        page.locator("#terms").check()
+
+        # to click link
+        page.get_by_role("link", name="terms and conditions").click()
+        # select drop down
+        teacher = page.get_by_role("combobox").select_option("Teacher")
+        print(teacher)
+        page.get_by_role("button", name="sign in").click()
+
+        # to validate assertion with test case
+        expect(page.get_by_title("ProtoCommerce")).to_be_visible()
 def test_diff_function():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
