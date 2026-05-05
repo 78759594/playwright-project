a3b64e3de941fba96caf7af3797c10e15c3ceab1
78759594 <shrikantnavale333@gamil.com>
2026-05-05 12:21:06 +0530

    new chages

--- tests/test_moreValidation.py
+++ tests/test_moreValidation.py
@@ -13,6 +13,13 @@ def test_diff_function():
         page.get_by_role("button",name="Hide").click()
         expect(page.get_by_placeholder("Hide/Show example")).to_be_hidden()
 
+        # mouse hover action
+        #page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+        page.locator("#mousehover").hover()
+        page.get_by_role("link",name="Top").click()
+
+
+
         # alert handle
         page.on("dialog",lambda dialog:dialog.accept())
         page.get_by_role("button",name="Confirm").click()
@@ -22,3 +29,17 @@ def test_diff_function():
         pageFrame = page.frame_locator("#courses-iframe")
         pageFrame.get_by_role("link", name="All Access plan").click()
         expect(pageFrame.locator("body")).to_contain_text("Happy Subscibers")
+
+        # check price of rice is equal to 30 from table
+        page.goto("https://rahulshettyacademy.com/seleniumPractice/#/offers")
+        for index in range(page.locator("th").count()):
+            if page.locator("th").nth(index).filter(has_text="Price").count()>0:
+                colValue = index
+                print(f"price col value is {colValue}")
+                break
+
+        riceRow = page.locator("tr").filter(has_text="Rice")
+        expect(riceRow.locator("td").nth(colValue)).to_have_text("37")
+
+
+        
